2ba4c65ac806d14feeec1a1217d4a1c1e982bacd
pouoq <3323249236@qq.com>
2025-08-21 00:45:37 +0800

    test local

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -22,7 +22,9 @@ public class Arithmetic {
     public static int sum(int a, int b) {
         return a + b;
     }
-
+    /**
+     * test local
+     **/
     public static void main(String[] args) {
         System.out.println("Give me a number! (no decimals, please)");
         int num1 = StdIn.readInt();
